155e46ad8bf61f124c4b085af8fa9ccb9373fb1a
Johan Stille <johan@withaspoon.se>
2011-03-23 12:05:24 +0100

    Added another edge test to single node partitioning.

--- tests/unit/test_consistent_hashing_partitioning_strategy.py
+++ tests/unit/test_consistent_hashing_partitioning_strategy.py
@@ -9,12 +9,14 @@ class HashCodeGeneratorMock(object):
         return self._key_hash_code_pairs[key]
 
 class TestWithOneNode(unittest.TestCase):
-    def test_should_return_that_node_for_any_key(self):
+    def test_should_return_a_single_node_for_any_key(self):
         hash_code_generator = HashCodeGeneratorMock({
+            "abc": "50",
             "node1.example:1234#0": "100",
-            "abc": "200"})
+            "def": "200"})
         partitioner = ConsistentHashingPartitioningStrategy(["node1.example:1234"], 1, hash_code_generator)
         self.assertItemsEqual(["node1.example:1234"], partitioner.find_node_addresses_for_key("abc"))
+        self.assertItemsEqual(["node1.example:1234"], partitioner.find_node_addresses_for_key("def"))
 
 class TestWithSeveralNodes(unittest.TestCase):
     def given_a_partitioning_strategy_with(self, replicas):
